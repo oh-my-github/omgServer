7744df05025f52888230c8b36911d4b6bcc09d43
Camry(황준호) <tocology@coupang.com>
2016-07-15 20:16:32 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Oh-My-Github Server
+
---
+++ src/test/java/com/ohmygithub/OmgServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ohmygithub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OmgServerApplication.class)
+public class OmgServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
